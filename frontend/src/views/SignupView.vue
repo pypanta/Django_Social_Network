daa288aedeb57e06a8cfe2c9a40fa23c6c82d1d1
Aleksandar Pantic <acapantic21@gmail.com>
2023-06-28 18:12:17 +0200

    Signup and login pages markup and styling

---
+++ frontend/src/views/SignupView.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="auth">
+    <div>
+      <h2>Sign Up</h2>
+      <p>Dolor veritatis iste excepturi velit delectus. Consequatur quasi nisi earum temporibus vero id harum Est perferendis at nobis animi nesciunt. Vel voluptatum odio earum similique officiis eveniet placeat.</p>
+      <p>Amet magnam aperiam magnam eligendi sunt Ut eaque eius magnam fugiat maxime Assumenda sapiente iste odio perferendis libero Enim tempore quos totam laboriosam consectetur. Dignissimos eum dolores soluta cum eligendi.</p>
+      <p>
+        Already have an account?
+        <router-link :to="{ name: 'login'}">Click here</router-link> to log in!
+      </p>
+    </div>
+    <div>
+      <form class="auth-form">
+        <div class="auth-form-group">
+          <label for="">Name</label>
+          <input type="text" placeholder="Your name">
+        </div>
+        <div class="auth-form-group">
+          <label for="">E-mail</label>
+          <input type="email" placeholder="Your e-mail address">
+        </div>
+        <div class="auth-form-group">
+          <label for="">Password</label>
+          <input type="password" placeholder="Your password">
+        </div>
+        <div class="auth-form-group">
+          <label for="">Repeat password</label>
+          <input type="password" placeholder="Repeat your password">
+        </div>
+        <input type="submit" value="Sign Up" class="btn btn-success">
+      </form>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
